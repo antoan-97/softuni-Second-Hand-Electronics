92814b374572edcbabc25990e7b5690690b73572
antoan-97 <antoan.todorov@abv.bg>
2024-06-01 14:29:04 +0300

    create buy manager

--- src/managers/electronicManager.js
+++ src/managers/electronicManager.js
@@ -3,3 +3,21 @@ const Electronic = require('../models/Electronic');
 exports.create = (electronicData) => Electronic.create(electronicData);
 
 exports.getAll = () => Electronic.find().populate('owner');
+
+exports.getOne = (electronicsId) => Electronic.findById(electronicsId).populate('owner');
+
+exports.buy = async (electronicId, userId) => {
+    const electronic = await Electronic.findById(electronicId);
+
+    if (!electronic) {
+        throw new Error('Electronic not found');
+    }
+
+    // Check if the user has already voted
+    if (electronic.buyingList.includes(userId)) {
+        return;
+    }
+
+    electronic.buyingList.push(userId);
+    return electronic.save();
+};

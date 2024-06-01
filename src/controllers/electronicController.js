5e34013913b9931575ad8fa490ccf96ded575a91
antoan-97 <antoan.todorov@abv.bg>
2024-06-01 14:28:56 +0300

    create buy details and buy controllers

--- src/controllers/electronicController.js
+++ src/controllers/electronicController.js
@@ -4,7 +4,7 @@ const { getErrorMessage } = require('../utils/errorHelper');
 
 
 
-router.get('/', async (req,res) =>{
+router.get('/', async (req, res) => {
     const electronics = await electronicManager.getAll().lean();
     res.render('electronics', { electronics })
 })
@@ -20,11 +20,39 @@ router.post('/create', async (req, res) => {
     }
 
     try {
-      await electronicManager.create(electronicData);
-      res.redirect('/electronics')
+        await electronicManager.create(electronicData);
+        res.redirect('/electronics')
     } catch (err) {
-     res.render('electronics/create', { error: getErrorMessage(err) });
+        res.render('electronics/create', { error: getErrorMessage(err) });
     }
 });
+
+router.get('/:electronicId/details', async (req, res) => {
+
+    const { user } = req
+    const electronicId = req.params.electronicId;
+
+    const electronic = await electronicManager.getOne(electronicId).lean();
+    const isOwner = req.user?._id == electronic.owner?._id;
+    const hasBuyed = electronic.buyingList?.some((v) => v?.toString() === user?._id);
+
+    res.render('electronics/details', { isOwner, electronic, user, hasBuyed });
+
+});
+
+
+
+router.get('/:electronicId/buy', async (req, res) => {
+    const electronicId = req.params.electronicId;
+    const userId = req.user._id;
+
+    try {
+        await electronicManager.buy(electronicId, userId);
+        res.redirect(`/electronics/${electronicId}/details`);
+    } catch (err) {
+        res.render('404', { error: getErrorMessage(err) });
+    }
+});
+
 
 module.exports = router;

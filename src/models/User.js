2957365999ae40275b7d7484c6af48a715dc4bf2
antoan-97 <antoan.todorov@abv.bg>
2024-06-01 14:49:14 +0300

    add validation for User schema

--- src/models/User.js
+++ src/models/User.js
@@ -6,14 +6,17 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Username is required!'],
         unique: true,
+        minLength: [3, 'Username should be at least 3 characters long!'],
     },
     password: {
         type: String,
         required:  [true, 'Password is required!'],
+        min: [4, 'Passwod should be at least 4 characters long!'],
     },
     email: {
         type: String,
         required: [true, 'Email is required!'],
+        minLength: [10, 'Email should be at least 10 characters long!'],
     },
 });
 

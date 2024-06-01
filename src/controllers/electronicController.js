9e936afa601076854ec39e02d8c21156daaeacda
antoan-97 <antoan.todorov@abv.bg>
2024-06-01 13:35:12 +0300

    add create functionality

--- src/controllers/electronicController.js
+++ src/controllers/electronicController.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
+const electronicManager = require('../managers/electronicManager');
+const { getErrorMessage } = require('../utils/errorHelper');
 
 
-router.get('/create', (req,res) =>{
-   res.render('electronics/create');
+
+router.get('/create', (req, res) => {
+    res.render('electronics/create');
+});
+
+router.post('/create', async (req, res) => {
+    const electronicData = {
+        ...req.body,
+        owner: req.user._id,
+    }
+
+    try {
+      await electronicManager.create(electronicData);
+      res.redirect('/electronics')
+    } catch (err) {
+     res.render('electronics/create', { error: getErrorMessage(err) });
+    }
 });
 
 module.exports = router;
